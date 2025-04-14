a597420c7c0345b11802c7e548db40d3e5c7b460
Himanshu <himanshutawer34@gmail.com>
2025-04-14 16:14:42 +0530

    vite build

--- backend/index.js
+++ backend/index.js
@@ -24,10 +24,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-// CORS configuration (hardcoded for local dev)
 const corsOptions = {
-  origin: "http://localhost:5173",  // Hardcoded URL for local dev
-  methods: "GET,POST,PUT,DELETE",  // Allowed HTTP methods
+  origin: [ "http://localhost:5173"],
+  methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
   allowedHeaders: "Content-Type,Authorization",
   credentials: true,
 };
@@ -40,7 +39,6 @@ app.use("/api/v1/message", messageRoute);
 app.use("/api/v1/task", taskRoute);
 app.use("/api/v1/chat", chatRoute);
 
-// Serve static files from the "Frontend" build directory
 app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 app.get("*", (_, res) => {
   res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
